a1dd87a6bdb61df83b67cd776cf40ff4869af092
Cameron Gross <camiam144@gmail.com>
2015-01-07 16:47:40 -0600

    testing with kitties

--- imgur-api.py
+++ imgur-api.py
@@ -2,5 +2,24 @@
 # Hopefully I'll also learn some coding stuff.
 
 
-import requests
+import urllib.request
+
+kitty = 'http://placekitten.com/'
+
+try:
+    response = urllib.request.urlopen(kitty)
+    kittens = response.read()
+    body = kittens[559:1000]
+
+    with open('temp.txt', 'w') as out_file:
+        out_file.write(str(body))
+            #out_file.write(line)
+
+    with open('temp.txt', 'r') as in_file:
+        text = in_file.read()
+
+    print(body)
+
+except urllib.request.URLError as e:
+    print('no kitties, error: ', e)
 
