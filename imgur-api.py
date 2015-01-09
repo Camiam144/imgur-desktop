8d8441b9abc18efee85ec4ccd1ae4edaec2945f4
Cameron Gross <camiam144@gmail.com>
2015-01-09 14:27:04 -0600

    Can't figure out how to change desktop background

--- imgur-api.py
+++ imgur-api.py
@@ -1,60 +1,1 @@
-# This should change my desktop background to today's top wallpaper post from imgur.
-# Hopefully I'll also learn some coding stuff.
-
-
-import urllib.request
-import json
-import random
-
-'''
-
-wallpapers = 'http://www.reddit.com/r/wallpapers/top.json?t=week&limit=25'
-
-
-try:
-    response = urllib.request.urlopen(wallpapers, timeout=5)
-    content = response.read()
-    json_data = json.loads(content.decode('utf-8'))
-
-
-except urllib.request.URLError as e:
-    print('Everything sucks, error: ', e)
-
-with open('tempout.txt', 'w') as out_file:
-    out_file.write(json.dumps(json_data, indent=4))
-
-'''
-
-with open('tempout.txt', 'r') as in_file:
-        json_data = json.load(in_file)
-
-
-# define a function that navigates to the random wallpaper and check if it's from imgur.
-# input data must be a json
-
-def reddit_paper(data):
-
-    number = random.randrange(0, 25)
-
-    trial_domain = data["data"]["children"][number]["data"]["domain"]
-
-    if trial_domain == 'i.imgur.com':
-        wallpaper_url = data["data"]["children"][number]["data"]["url"]
-        return wallpaper_url
-
-imgur_url = reddit_paper(json_data)
-
-print(imgur_url)
-
-
-def get_from_imgur(input_url):
-    try:
-        data = urllib.request.urlopen(input_url, timeout=5).read()
-        if len(data) > 0:
-            with open('wallpaper.jpg', 'wb') as out_file:
-                out_file.write(data)
-
-    except urllib.request.URLError as e:
-        print('Error, no image saved: ', e)
-
-get_from_imgur(imgur_url)
+# This should change my desktop background to today's top wallpaper post from imgur.# Hopefully I'll also learn some coding stuff.import urllib.requestimport jsonimport randomimport subprocess'''wallpapers = 'http://www.reddit.com/r/wallpapers/top.json?t=week&limit=25'try:    response = urllib.request.urlopen(wallpapers)    content = response.read()    json_data = json.loads(content.decode('utf-8'))except urllib.request.URLError as e:    print('Everything sucks, error: ', e)with open('tempout.txt', 'w') as out_file:    out_file.write(json.dumps(json_data, indent=4))'''with open('tempout.txt', 'r') as in_file:        json_data = json.load(in_file)# define a function that navigates to the random wallpaper and check if it's from imgur.# input data must be a jsondef reddit_paper(data):    number = random.randrange(0, 25)    trial_domain = data["data"]["children"][number]["data"]["domain"]    if trial_domain == 'i.imgur.com':        wallpaper_url = data["data"]["children"][number]["data"]["url"]        return wallpaper_urldef get_from_imgur(input_url):    try:        data = urllib.request.urlopen(input_url).read()        if len(data) > 0:            with open('wallpaper.jpg', 'wb') as out_file:                out_file.write(data)    except urllib.request.URLError as e:        print('Error, no image saved: ', e)SCRIPT = """/usr/bin/osascript<<ENDtell application "Finder"set desktop picture to POSIX file ./wallpaper.jpgend tellEND"""def change_background():    subprocess.Popen(SCRIPT, shell=True)def main():    url = reddit_paper(json_data)    print(url)    get_from_imgur(url)    change_background()main()
